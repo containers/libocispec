96308f0a28e2ae6e25dec907d306f145b768c17c
Giuseppe Scrivano <gscrivan@redhat.com>
2017-03-19 13:52:32 +0100

    tests: add some tests
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

---
+++ tests/test-2.c
@@ -0,0 +1,54 @@
+/* Copyright (C) 2017 Giuseppe Scrivano <giuseppe@scrivano.org>
+
+libocispec is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+libocispec is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with libocispec.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "config.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "spec.h"
+
+int
+main (int argc, char *argv[])
+{
+  size_t rd;
+  yajl_val tree;
+  FILE *config;
+  char *file_data = malloc (1024 * 1024);
+  char errbuf[1024];
+
+  printf("%s\n", getcwd(NULL,0));
+  config = fopen ("tests/config.nocwd.json", "r+");
+  if (config == NULL)
+    exit (1);
+  rd = fread(file_data, 1, 1024 * 1024 - 1, config);
+  if (rd < 0)
+    exit (2);
+  fclose (config);
+
+  tree = yajl_tree_parse (file_data, errbuf, sizeof(errbuf));
+  if (tree == NULL)
+    exit (3);
+
+  oci_parser_error err;
+  oci_container_container *container = make_oci_container_container (tree, &err);
+
+  if (container != NULL || err != OCI_PARSER_ERROR_REQUIRED_FIELD_NOT_PRESENT) {
+    exit (4);
+  }
+  exit (0);
+}
