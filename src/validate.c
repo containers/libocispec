ff888a745458154c2e157be2ffd33587b02b70d5
Giuseppe Scrivano <gscrivan@redhat.com>
2017-07-07 17:45:15 +0200

    validate: add a small tool for quick verification of OCI spec files
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

---
+++ src/validate.c
@@ -0,0 +1,44 @@
+/* Copyright (C) 2017 Giuseppe Scrivano <giuseppe@scrivano.org>
+
+libocispec is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+libocispec is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with libocispec.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "config.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <error.h>
+#include "spec.h"
+
+int
+main (int argc, char *argv[])
+{
+  oci_parser_error err;
+  oci_container_container *container;
+  const char *file = "config.json";
+
+  if (argc > 1)
+    file = argv[1];
+
+  container = oci_parse_file (file, LIBOCISPEC_OPTIONS_STRICT, &err);
+  if (container)
+    free_oci_container_container (container);
+
+  if (err)
+    error (EXIT_FAILURE, 0, "error in %s: %s", file, err);
+
+  exit (0);
+}
