ced8184bc8a5f5acb13226d3c979e62ee30dc9a2
0x0916 <w@laoqinren.net>
2017-07-25 16:04:59 +0800

    tests: add test-3 to test oci_image_spec.{h|c}
    Signed-off-by: 0x0916 <w@laoqinren.net>

---
+++ tests/test-3.c
@@ -0,0 +1,51 @@
+/* Copyright (C) 2017 Wang Long <w@laoqinren.net>
+
+libocispec is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+libocispec is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with libocispec.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "config.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "oci_image_spec.h"
+
+int
+main (int argc, char *argv[])
+{
+  oci_parser_error err;
+  oci_image_image *image = oci_image_parse_file ("tests/image_config.json", 0, &err);
+
+  if (image == NULL) {
+    printf ("error %s\n", err);
+    exit (1);
+  }
+  if (strcmp (image->author, "Alyssa P. Hacker <alyspdev@example.com>"))
+    exit (5);
+  if (strcmp (image->created, "2015-10-31T22:22:56.015925234Z"))
+    exit (5);
+  if (strcmp (image->os, "linux"))
+    exit (5);
+  if (strcmp (image->architecture, "amd64"))
+    exit (5);
+  if (strcmp (image->config->User, "1:1"))
+    exit (5);
+  if (strcmp (image->config->Env[1], "FOO=docker_is_a_really"))
+    exit (5);
+  if (strcmp (image->config->Entrypoint[0], "/bin/sh"))
+    exit (5);
+  free_oci_image_image (image);
+  exit (0);
+}
