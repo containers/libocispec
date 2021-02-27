c4ee4d6acbce1e2d2c5c1cfba8727c872040d3d0
Giuseppe Scrivano <gscrivan@redhat.com>
2021-02-27 22:05:53 +0100

    validate: support persistent fuzzing mode
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- src/validate.c
+++ src/validate.c
@@ -22,6 +22,46 @@ along with libocispec.  If not, see <http://www.gnu.org/licenses/>.
 #include <string.h>
 #include "runtime_spec_schema_config_schema.h"
 
+#ifdef FUZZER
+int
+LLVMFuzzerInitialize (int *argc, char ***argv)
+{
+  return 0;
+}
+
+int
+LLVMFuzzerTestOneInput (uint8_t *buf, size_t len)
+{
+  runtime_spec_schema_config_schema *container;
+  struct parser_context ctx;
+  parser_error err;
+  FILE *s;
+
+  if (len == 0)
+    return 0;
+
+  s = fmemopen (buf, len, "r");
+  if (s == NULL)
+    return 0;
+
+  ctx.options = OPT_PARSE_STRICT;
+  ctx.errfile = stderr;
+  container = runtime_spec_schema_config_schema_parse_file_stream (s, &ctx, &err);
+  fclose (s);
+  if (container)
+    {
+      free_runtime_spec_schema_config_schema (container);
+      return 0;
+    }
+  if (err)
+    {
+      fprintf (stderr, "error: %s\n", err);
+      free (err);
+    }
+  return 0;
+}
+#endif
+
 int
 main (int argc, char *argv[])
 {
@@ -29,6 +69,22 @@ main (int argc, char *argv[])
   runtime_spec_schema_config_schema *container;
   const char *file = "config.json";
   struct parser_context ctx;
+
+#ifdef FUZZER
+  if (getenv ("VALIDATE_FUZZ"))
+    {
+      extern void HF_ITER (uint8_t** buf, size_t* len);
+      for (;;)
+        {
+          size_t len;
+          uint8_t *buf;
+
+          HF_ITER (&buf, &len);
+
+          LLVMFuzzerTestOneInput (buf, len);
+	}
+    }
+#endif
 
   if (argc > 1)
     file = argv[1];
@@ -43,6 +99,7 @@ main (int argc, char *argv[])
   if (err)
     {
       fprintf (stderr, "error in %s: %s\n", file, err);
+      free (err);
       exit (EXIT_FAILURE);
     }
 
