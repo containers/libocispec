544a0ed6c673e1839937300c045de78a0645aaf0
Sourav Moitra <sourav.moitr@gmail.com>
2024-12-19 21:39:51 +0530

    Calculate of len on residual
    Signed-off-by: Sourav Moitra <sourav.moitr@gmail.com>

--- src/ocispec/json_common.h
+++ src/ocispec/json_common.h
@@ -230,7 +230,7 @@ typedef struct
 
 jansson_object_keys_values *json_object_to_keys_values(json_t *object);
 
-json_t *copy_unmatched_fields(json_t *src, const char **exclude_keys);
+json_t *copy_unmatched_fields(json_t *src, const char **exclude_keys, int len);
 
 #ifdef __cplusplus
 }
